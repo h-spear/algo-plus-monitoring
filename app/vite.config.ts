da7bdd3045167f45b7353bf2fff5e67acf4919b2
h-spear <80024307+h-spear@users.noreply.github.com>
2025-06-28 01:47:07 +0900

    Chore: vite.config.js 수정

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
+    base: '/algo-plus-monitoring/',
     plugins: [react(), tailwindcss()],
     server: {
         port: 3000,
